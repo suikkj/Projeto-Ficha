08a7b27ef99ec1f0001fb927c210a9779b4cb2df
Lucas Oliveira <125172187+suikkj@users.noreply.github.com>
2025-03-13 13:21:12 -0300

    Add files via upload

--- models/Ficha.js
+++ models/Ficha.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const classesData = require('../config/classesData');   
+
+
+
 
 const atributosSchema = new mongoose.Schema({
     forca: {type: Number, default: 0},
@@ -18,10 +22,23 @@ const periciaSchema = new mongoose.Schema({
 
 }); 
 
+const habilidadeSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    descricao:{
+        type: String,
+        required: true,
+    }
+})
+
+
+
 const fichaSchema = new mongoose.Schema({
     userId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Usuario',
         required: true,
     },
     raca:{
@@ -49,17 +66,35 @@ const fichaSchema = new mongoose.Schema({
     atributos: {
         type: atributosSchema, default: () => ({}) },
 
-    pontosDeVidaMax: {type: Number, default: 0},
-    pontosDeManaMax: {type: Number, default: 0},
-    pontosDeVidaAtual: {type: Number, default: 0},
-    pontosDeManaAtual: {type: Number, default: 0},
+    pontosDeVidaMax: {
+        type: Number,
+        required: true, 
+        default: 0
+    },
+    pontosDeManaMax: {
+        type: Number, 
+        required: true,
+        default: 0
+    },
+    pontosDeVidaAtual: {
+        type: Number,
+        required: true, 
+        default: 0
+    },
+    pontosDeManaAtual: {
+        type: Number,
+        required: true, 
+        default: 0
+    },
+
     pericias: {
         type: [String],
         default: [],
     },
     identidade: {
-        nomePersonagem: {type: String, required: true, default: 'Nome Padrão'},
+        nomePersonagem: {type: String, required: true, default: 'Insurgente'},
     },
+    habilidades: [habilidadeSchema],
     fotoPersonagem:{type: String},
     defesa:{type: Number, default: 0},
     cdMagia:{type: Number, default: 0},
@@ -67,6 +102,46 @@ const fichaSchema = new mongoose.Schema({
     divindade:{type: String},
 });
 
+fichaSchema.methods.adicionarHabilidade = function(habilidadeNome){
+    if(!this.habilidades.some(hab => hab.nome.toLowerCase() === habilidadeNome.toLowerCase())){
+        this.habilidades.push({nome: habilidadeNome.trim()});
+    }
+};
 
+fichaSchema.methods.removerHabilidade = function(habilidadeNome){
+    const index = this.habilidades.findIndex(
+        hab => hab.nome.toLowerCase() === habilidadeNome.toLowerCase()
+    );
+    if(index !== -1){
+        this.habilidades.splice(index, 1);
+        return true;
+    }
+    return false;
+};
+
+fichaSchema.pre('save', function(next){
+    if(this.isModified('classe') || this.isModified('atributos') || this.isModified('nivel')){
+        console.log(`Classe sendo salva: ${this.classe}`);
+        const classeInfo = classesData[this.classe];
+        console.log(`Informações da classe:`, classeInfo);
+
+        if(classeInfo){
+            this.pontosDeVidaMax = classeInfo.basePV + (this.nivel - 1) * classeInfo.pvPorNivel;
+            this.pontosDeManaMax = classeInfo.basePM + (this.nivel - 1) * classeInfo.pmPorNivel;
+            console.log(`pontosDeVidaMax definido: ${this.pontosDeVidaMax}`);
+            console.log(`pontosDeManaMax definido: ${this.pontosDeManaMax}`);
+        } else{
+            console.log(`Classe não encontrada: ${this.classe}`);
+            this.pontosDeVidaMax = 0;
+            this.pontosDeManaMax = 0;
+        }
+
+        this.pontosDeVidaAtual = Math.min(this.pontosDeVidaAtual, this.pontosDeVidaMax);
+        this.pontosDeManaAtual = Math.min(this.pontosDeManaAtual, this.pontosDeManaMax);
+        console.log(`pontosDeVidaAtual após ajuste: ${this.pontosDeVidaAtual}`);
+        console.log(`pontosDeManaAtual após ajuste: ${this.pontosDeManaAtual}`);
+    }
+    next();
+});
 
 module.exports = mongoose.model('Ficha', fichaSchema);
