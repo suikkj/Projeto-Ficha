f52016af932cef920db2f9d755f49f1c2353b7f6
Lucas Oliveira <125172187+suikkj@users.noreply.github.com>
2025-03-13 13:20:23 -0300

    Add files via upload

---
+++ config/classesData.js
@@ -0,0 +1,95 @@
+const classesData = {
+    'Arcanista': {
+        basePV: 8,
+        pvPorNivel: 2,
+        basePM: 6,
+        pmPorNivel: 6
+    },
+    'Barbaro': { 
+        basePV: 24,
+        pvPorNivel: 6,
+        basePM: 3,
+        pmPorNivel: 3
+    },
+    'Bardo': {
+        basePV: 12,
+        pvPorNivel: 3,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Bucaneiro': {
+        basePV: 16,
+        pvPorNivel: 4,
+        basePM: 3,
+        pmPorNivel: 3
+    },
+    'Cacador': { 
+        basePV: 16,
+        pvPorNivel: 4,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Cavaleiro': {
+        basePV: 20,
+        pvPorNivel: 5,
+        basePM: 3,
+        pmPorNivel: 3
+    },
+    'Clerigo': { 
+        basePV: 16,
+        pvPorNivel: 4,
+        basePM: 5,
+        pmPorNivel: 5
+    },
+    'Druida': {
+        basePV: 16,
+        pvPorNivel: 4,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Frade': {
+        basePV: 12,
+        pvPorNivel: 3,
+        basePM: 6,
+        pmPorNivel: 6
+    },
+    'Guerreiro': {
+        basePV: 20,
+        pvPorNivel: 5,
+        basePM: 3,
+        pmPorNivel: 3
+    },
+    'Inventor': {
+        basePV: 12,
+        pvPorNivel: 3,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Ladino': {
+        basePV: 12,
+        pvPorNivel: 3,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Lutador': {
+        basePV: 20,
+        pvPorNivel: 5,
+        basePM: 3,
+        pmPorNivel: 3
+    },
+    'Nobre': {
+        basePV: 16,
+        pvPorNivel: 4,
+        basePM: 4,
+        pmPorNivel: 4
+    },
+    'Paladino': {
+        basePV: 20,
+        pvPorNivel: 5,
+        basePM: 3,
+        pmPorNivel: 3
+    }
+    
+};
+
+module.exports = classesData;
